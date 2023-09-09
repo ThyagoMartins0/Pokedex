81badc50a6c012b63aa1e12e3b2672471a6ea083
Thyago Ramos <oficialthyago2@gmail.com>
2023-09-09 18:05:37 -0300

    deixando o codigo enxuto

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,16 +3,9 @@ const limit = 10
 const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
 fetch(url)
-    /*SOLICITAR REQUISIÇÃO DE API "TUDO CERTO" */
-    .then(function(response){
-    console.log(response)
-    })
-    /*SOLICITAR REQUISIÇÃO DE API "CASO ALGO DEE ERRADO" */
-    .catch(function(error){
-        console.log(error)
-    })
-    /*SOLICITAR REQUISIÇÃO DE API "INDENPEDENTE SE DE CERTO OU ERRADO MOSTRE QUE A REQUISIÇÃO FOI CONCLUIDA" */
-    .finally(function(){
-        console.log("Requisição concluida!")
-    })
+/*SOLICITAR REQUISIÇÃO DE API "TUDO CERTO" */
+    .then((response) => response.json())
+    .then((jsonBody) => console.log(jsonBody))
 
+    /*SOLICITAR REQUISIÇÃO DE API "CASO ALGO DEE ERRADO" */
+    .catch((error) => console.error(error))
