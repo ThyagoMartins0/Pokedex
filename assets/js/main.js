1d3345db46b8de3f9db8a4e45953ecc1b738b677
Thyago Christian Ramos Martins <thyago.martins@esamcsantos.onmicrosoft.com>
2023-09-10 00:42:59 -0300

    removendo codigo bubassaur

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,17 @@
-
-
-const offset=0
-const limit = 10
-const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+function convertPokemonTypeToLi(pokemonType) {
+    return pokemonType.map((typeSlot) =>`<li class="type">${typeSlot.type.name}</li>`)
+}
 
 function convertPokemonToLi(pokemon){
     return ` 
     <li class="pokemon">
-    <span class="number">#001</span>
+    <span class="number">#${pokemon.order}</span>
     <span class="name">${pokemon.name}</span>
     <div class="detail">
         <ol class="types">
-            <li class="type">grass</li>
-            <li class="type">poison</li>
+        ${convertPokemonTypeToLi(pokemon.types).join('')}
         </ol>
-    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" 
+    <img src=${pokemon.sprites.other.dream_world.front_default}
     alt="${pokemon.name}">
     </div>
 </li>
@@ -24,11 +21,8 @@ function convertPokemonToLi(pokemon){
 const pokemonList = document.getElementById('pokemonList')
 
 pokeApi.getPokemons().then((pokemons) => {
-    const listItems =[]
 
-        for (let i = 0 ; i < pokemons.length;  i++){
-            const pokemon = pokemons[i];
-        listItems.push(convertPokemonToLi(pokemon))
-        }
-        console.log(listItems)
-    })
+  pokemonList.innerHTML += pokemons.map(convertPokemonToLi).join('')
+      
+  
+   }) 
